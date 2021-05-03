70e65b4f53a425d23b0579fa39e8ad77a63bda75
semih.saydam <semih.saydam@testinium.com>
2021-05-03 05:56:20 +0300

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println("Merhaba Medium !");
+    }
+
+    public int Add(int number1, int number2){
+        return number1 + number2;
+    }
+
+}
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
+/**
+ * @author Semih Saydam
+ * @version 0.0.1
+ */
+public class AppTest 
+{
+    @Test
+    public void AddTest() {
+        App app = new App();
+        int number1 = 3;
+        int number2 = 5;
+        int addResult = app.Add(number1,number2);
+        assertEquals
+                ("This is not equal!",
+                        8,addResult);
+    }
+
+    @Test
+    public void mainTest(){
+        App app = new App();
+        app.main(new String[] {"arg1","arg2"});
+    }
+}
