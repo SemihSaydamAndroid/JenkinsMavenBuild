bc56596cf4a4aee57fa062a78c7d5aa70e93dd56
semih.saydam <semih.saydam@testinium.com>
2021-05-05 07:26:07 +0300

    disrupt the unit test for deploy test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -19,7 +19,7 @@ public class AppTest
         int addResult = app.Add(number1,number2);
         assertEquals
                 ("This is not equal!",
-                        8,addResult);
+                        10,addResult);
     }
 
     @Test
