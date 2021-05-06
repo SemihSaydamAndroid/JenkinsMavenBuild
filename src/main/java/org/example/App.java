f86b478ac1447dc25d410f588adf20aad9e38a8b
semih.saydam <semih.saydam@testinium.com>
2021-05-06 06:02:21 +0300

    three unit test added for surfire reports

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,4 +18,12 @@ public class App
         return number1 + number2;
     }
 
+    public int Subtract(int number1, int number2){
+        return number1 - number2;
+    }
+
+    public int Multiply(int number1, int number2){
+        return number1 * number2;
+    }
+
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -9,22 +9,33 @@ import org.junit.Test;
  * @author Semih Saydam
  * @version 0.0.1
  */
-public class AppTest 
-{
+public class AppTest {
+    App app = new App();
+    int number1 = 3;
+    int number2 = 5;
+
     @Test
     public void AddTest() {
-        App app = new App();
-        int number1 = 3;
-        int number2 = 5;
-        int addResult = app.Add(number1,number2);
-        assertEquals
-                ("This is not equal!",
-                        10,addResult);
+        int addResult = app.Add(number1, number2);
+        assertEquals("This is not equal!",
+                        8, addResult);
     }
 
     @Test
-    public void mainTest(){
+    public void SubtractTest() throws Exception {
+        int SubResult = app.Subtract(number2,number1);
+        assertEquals(2,SubResult);
+    }
+
+    @Test
+    public void MultiplyTest() throws Exception {
+        int MultipResult = app.Multiply(number2,number1);
+        assertEquals(15,MultipResult);
+    }
+
+    @Test
+    public void mainTest() {
         App app = new App();
-        app.main(new String[] {"arg1","arg2"});
+        app.main(new String[]{"arg1", "arg2"});
     }
 }
